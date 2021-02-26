a8dbd0b98b581570e4be8c1ecb173685a28a5585
Souad Allahham <souadallahham89@gmail.com>
2021-02-26 18:05:58 +0100

    I finished ex-3

--- ex-3/exercise.js
+++ ex-3/exercise.js
@@ -1,3 +1,32 @@
 // write your JS here
 // remember to always test in the browser
 // to see if everything works correctly
+
+/*the following array represents my top choice in colors, food, hobbies and pet.*/
+
+let topChoice = ["purple", "pizza", "writing", "cat"]
+console.log(" My #1 color is "+ topChoice[0]);
+console.log(" My #1 food is "+ topChoice[1]);
+console.log(" My #1 hobbies is "+ topChoice[2]);
+console.log(" My #1 pet is "+ topChoice[3]);
+
+let myPet = {
+    name: "tigro",
+    Type: "cat",
+  }; 
+  
+window.alert("My favorite pet is "+ myPet.Type+ " and his name is "+ myPet.name); 
+
+let myFood = {
+    name: "pizza",
+    Type: "parmigiana",
+  }; 
+
+window.alert("My favorite food is "+ myFood.name+ " and its type is "+ myFood.Type);
+  
+let myHobby = {
+    name: "writing",
+    Type: "poems",
+  }; 
+
+  window.alert("My hobby is "+ myHobby.name+ " "+ myHobby.Type);
