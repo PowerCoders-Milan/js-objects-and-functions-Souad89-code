7b25e3e4620304ec212779738b25f9704635b607
Souad Allahham <souadallahham89@gmail.com>
2021-02-26 12:37:42 +0100

    I finished ex-1

--- ex-1/exercise.js
+++ ex-1/exercise.js
@@ -1,3 +1,13 @@
 // write your JS here
 // remember to always test in the browser
 // to see if everything works correctly
+
+function sentence(noun, verb, adverb, adjective) {
+    return noun + verb + adverb + adjective;
+  }
+/*console.log(sentence("Giulia ", "is ", "really ", "beautiful "));*/
+let noun = prompt("please insert the noun!");
+let verb = prompt("please insert the verb!");
+let adverb = prompt("please insert the adverb!");
+let adjective = prompt("please insert the adjective!");
+window.alert(sentence(noun +" " , verb +" " , adverb +" " , adjective + "."));
